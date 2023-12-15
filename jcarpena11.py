a6aba939c739172d59602051a798cfceb437c336
Jonathan Carpena <jonathancarpena99@gmail.com>
2023-12-15 00:33:41 +0000

    Adding a python program

---
+++ jcarpena11.py
@@ -0,0 +1,10 @@
+# Jonathan Carpena
+# CSC321
+# Fall2023
+
+user_input = float(input("Enter a number: "))
+
+result = user_input ** 2
+
+print(f"Your number squared is {result} Well done is better than well said.")
+
